3e1927eb5a1a57062eefb93697beab12d7e61c74
Daniel Hornik <daniel.1302@gmail.com>
2022-06-14 20:34:28 -0500

    feat: simplify config by adding download-config prestart hook

--- stagnet3/vegacapsule/script.sh
+++ stagnet3/vegacapsule/script.sh
@@ -8,19 +8,17 @@ export NOMAD_TLS_SERVER_NAME="server.global.nomad"
 export AWS_PROFILE=vega
 
 
-
 # pushd /Users/daniel/www/vega/vega;
 # GOOS=linux GOARCH=amd64 go build -o vega ./cmd/vega
 # aws s3 cp vega s3://vegacapsule-test/bin/vega-linux-amd64-v0.50.2-build
 # popd 
-
 
 pushd /Users/daniel/www/vega/vegacapsule/
 go build -o vegacapsule .
 cp vegacapsule /Users/daniel/www/vega/networks-internal/vegacapsule
 popd
 
-# ./vegacapsule network destroy --home-path ./stagnet3/vegacapsule/home
+./vegacapsule network destroy --home-path ./stagnet3/vegacapsule/home
 ./vegacapsule network generate --force --config-path ./stagnet3/vegacapsule/config.hcl --home-path ./stagnet3/vegacapsule/home
 
 aws s3 rm --recursive s3://vegacapsule-test/stagnet3
