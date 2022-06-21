6314ca6aeb6db0383819582ef57e91eb85329f3c
Daniel Hornik <daniel.1302@gmail.com>
2022-06-20 19:10:58 -0500

    feat: add caddy server and refactor config for new vegacapsule config format

--- stagnet3/vegacapsule/script.sh
+++ stagnet3/vegacapsule/script.sh
@@ -19,13 +19,12 @@ cp vegacapsule /Users/daniel/www/vega/networks-internal/vegacapsule
 popd
 
 ./vegacapsule network destroy --home-path ./stagnet3/vegacapsule/home
-./vegacapsule network generate --force --config-path ./stagnet3/vegacapsule/config.hcl --home-path ./stagnet3/vegacapsule/home
-
-aws s3 rm --recursive s3://vegacapsule-test/stagnet3
-aws s3 cp --recursive ./stagnet3/vegacapsule/home/ s3://vegacapsule-test/stagnet3/
-
-./vegacapsule network start --home-path ./stagnet3/vegacapsule/home
+./vegacapsule network generate --force --config-path ./stagnet3/vegacapsule/config.hcl --home-path ./stagnet3/vegacapsule/home \
+    && aws s3 rm --recursive s3://vegacapsule-test/stagnet3 \
+    && aws s3 cp --recursive ./stagnet3/vegacapsule/home/ s3://vegacapsule-test/stagnet3/ \
+    && ./vegacapsule network start --home-path ./stagnet3/vegacapsule/home \
+    || echo " === FAILED === " 
 
 
 # Update nomad template
-./vegacapsule network start --home-path ./stagnet3/vegacapsule/home
+# ./vegacapsule network start --home-path ./stagnet3/vegacapsule/home
