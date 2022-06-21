e5ff8a57ed762b2487d37ce346dc28a4bbef909f
Daniel Hornik <daniel.1302@gmail.com>
2022-06-21 18:40:26 -0500

    feat: incorporate caddy server as part of a node-set job

--- stagnet3/vegacapsule/script.sh
+++ stagnet3/vegacapsule/script.sh
@@ -13,12 +13,16 @@ export AWS_PROFILE=vega
 # aws s3 cp vega s3://vegacapsule-test/bin/vega-linux-amd64-v0.50.2-build
 # popd 
 
-pushd /Users/daniel/www/vega/vegacapsule/
-go build -o vegacapsule .
-cp vegacapsule /Users/daniel/www/vega/networks-internal/vegacapsule
-popd
+# pushd /Users/daniel/www/vega/vegacapsule/
+# go build -o vegacapsule .
+# cp vegacapsule /Users/daniel/www/vega/networks-internal/vegacapsule
+# popd
 
-./vegacapsule network destroy --home-path ./stagnet3/vegacapsule/home
+
+./vegacapsule network stop --home-path ./stagnet3/vegacapsule/home \
+    && ./vegacapsule nodes unsafe-reset-all --remote --home-path ./stagnet3/vegacapsule/home \
+    && ./vegacapsule network destroy --home-path ./stagnet3/vegacapsule/home;
+    
 ./vegacapsule network generate --force --config-path ./stagnet3/vegacapsule/config.hcl --home-path ./stagnet3/vegacapsule/home \
     && aws s3 rm --recursive s3://vegacapsule-test/stagnet3 \
     && aws s3 cp --recursive ./stagnet3/vegacapsule/home/ s3://vegacapsule-test/stagnet3/ \
@@ -27,4 +31,6 @@ popd
 
 
 # Update nomad template
-# ./vegacapsule network start --home-path ./stagnet3/vegacapsule/home
+# ./vegacapsule network stop --home-path ./stagnet3/vegacapsule/home \
+#     && ./vegacapsule nodes unsafe-reset-all --remote --home-path ./stagnet3/vegacapsule/home \
+#     && ./vegacapsule network start --home-path ./stagnet3/vegacapsule/home
