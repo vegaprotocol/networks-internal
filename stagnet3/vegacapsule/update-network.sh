039a5208a22d1d8256dfecc52a31481fd03be4de
Daniel Hornik <daniel.1302@gmail.com>
2022-07-29 17:11:05 -0500

    feat: refactor configuration for node_set

--- stagnet3/vegacapsule/update-network.sh
+++ stagnet3/vegacapsule/update-network.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+
+unset AWS_PROFILE;
+export AWS_REGION="eu-west-2";
+export AWS_ACCESS_KEY_ID="AKIAVFM72G3TXBYO3P73";
+export AWS_SECRET_ACCESS_KEY="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";
+export S3_BUCKET_NAME="vegacapsule-20220722172637220400000001";
+
 export NOMAD_ADDR="https://n00.stagnet3.vega.xyz:4646";
 export NOMAD_CACERT="./certs/nomad-ca.pem";
 export NOMAD_CLIENT_CERT="./certs/client.pem";
 export NOMAD_CLIENT_KEY="./certs/client-key.pem";
 export NOMAD_TLS_SERVER_NAME="server.global.nomad";
 
-unset AWS_PROFILE;
-export AWS_REGION="us-east-1";
-export AWS_ACCESS_KEY_ID="AKIAVFM72G3TWXHWEFNB";
-export AWS_SECRET_ACCESS_KEY="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";
-export S3_BUCKET_NAME="vegacapsule-test";
 
 if ! aws --version > /dev/null 2>&1; then 
     echo "[ERROR] awscli is missing. Install it by following this docs: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html";
@@ -32,7 +34,7 @@ if ! data-node version > /dev/null 2>&1; then
     exit 1;
 fi;
 
-if ! aws s3 ls > /dev/null 2>&1; then
+if ! aws s3 ls "s3://${S3_BUCKET_NAME}" > /dev/null 2>&1; then
     echo "[ERROR] aws cli has no permissions for S3";
     exit 1;
 fi;
@@ -86,12 +88,12 @@ echo "[INFO] importing validators keys to the networks";
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
 vegacapsule network keys import --keys-file-path  ./network.json --home-path ./home
 
-vegacapsule template node-sets --with-merge \
-    --home-path ./home \
-    --path ./config/tendermint.validators.tmpl.toml \
-    --nodeset-group-name validators \
-    --type tendermint \
-    --update-network;
+# vegacapsule template node-sets --with-merge \
+#     --home-path ./home \
+#     --path ./config/tendermint.validators.tmpl.toml \
+#     --nodeset-group-name validators \
+#     --type tendermint \
+#     --update-network;
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^";
 echo "";
 sleep 3;
